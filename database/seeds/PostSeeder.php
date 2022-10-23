5b9fe2f15b1068720e8939b4cd08894e6295973e
FabriAtom <fabrizioromano20@gmail.com>
2022-10-23 12:50:35 +0200

    errore localhost

--- database/seeds/PostSeeder.php
+++ database/seeds/PostSeeder.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Post;
 use Illuminate\Database\Seeder;
-
+use Faker\Generator as Faker;
+use Illuminate\Support\str;
 class PostSeeder extends Seeder
 {
     /**
@@ -9,8 +11,17 @@ class PostSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        //
+        // $categoryIds = Category::all()->pluck('id');
+
+        for ($i = 0; $i < 50; $i++) {
+            $post = new Post();
+            $post->title = $faker->words(rand(5, 10), true);
+            $post->content = $faker->paragraphs(rand(10, 20), true);
+            $post->slug = Str::slug($post->title);
+            // $post->category_id = $faker->optional()->randomElement($categoryIds);
+            $post->save();
+        }
     }
 }
