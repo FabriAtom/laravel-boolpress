6aab8b59308dfabb4542ae87c2e432c164f90b40
FabriAtom <fabrizioromano20@gmail.com>
2022-10-24 22:59:48 +0200

    commit tags

--- app/Http/Controllers/Admin/PostController.php
+++ app/Http/Controllers/Admin/PostController.php
@@ -5,8 +5,8 @@ namespace App\Http\Controllers\Admin;
 use App\Category;
 use App\Http\Controllers\Controller;
 use App\Post;
+use App\Tag;
 use Illuminate\Http\Request;
-use Illuminate\Support\Str;
 
 class PostController extends Controller
 {
@@ -29,7 +29,8 @@ class PostController extends Controller
     public function create()
     {
         $categories = Category::orderBy('name', 'asc')->get();
-        return view('admin.posts.create', compact('categories'));
+        $tags = Tag::orderBy('name','asc')->get();
+        return view('admin.posts.create', compact('categories','tags'));
     }
 
     /**
@@ -44,12 +45,17 @@ class PostController extends Controller
             'title' => 'required|max:255|min:5',
             'content' => 'required',
             'category_id' => 'nullable|exists:categories,id',
+            'tags.*' => 'exists:tags,id',
         ]);
-        // $slug= Str::slug($params['title']);
-
-
+        
         $params['slug'] = Post::getUniqueSlugFromTitle($params['title']);
         $post = Post::create($params);
+       
+
+    if(array_key_exists('tags', $params)){
+        $tags = $params['tags'];
+        $post->tags()->sync($tags);
+    }
         return redirect()->route('admin.posts.show', $post);
     }
 
@@ -73,7 +79,8 @@ class PostController extends Controller
     public function edit(Post $post)
     {
         $categories = Category::orderBy('name', 'asc')->get();
-        return view('admin.posts.edit', compact('categories', 'post'));
+        $tags = Tag::orderBy('name','asc')->get();
+        return view('admin.posts.edit', compact('categories', 'post','tags'));
     }
 
     /**
@@ -88,7 +95,8 @@ class PostController extends Controller
         $params = $request->validate([
             'title' => 'required|max:255|min:5',
             'content' => 'required',
-            'category_id' => 'nullable|exists:categories,id'
+            'category_id' => 'nullable|exists:categories,id',
+            'tags.*' => 'exists:tags,id',
         ]);
 
         if ($params['title'] !== $post->title) {
@@ -96,6 +104,11 @@ class PostController extends Controller
         }
 
         $post->update($params);
+        if (array_key_exists('tags',$params)){
+            $post->tags()->sync($params['tags']);
+        }else{
+            $post->tags()->detach();
+        }
 
         return redirect()->route('admin.posts.show', $post);
     }
