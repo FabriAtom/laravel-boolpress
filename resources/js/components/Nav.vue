3ec1cbbfdc9b46253513c56d6ae593be89e4f04a
FabriAtom <fabrizioromano20@gmail.com>
2022-11-03 18:44:52 +0100

    add params slug

---
+++ resources/js/components/Nav.vue
@@ -0,0 +1,48 @@
+<template>
+  <header>
+    <nav class="container">
+        <div class="flex justify-between items-center">
+            <div>
+                LOGO
+            </div>
+            <ul class="flex items-center">
+                <li class="px-2 py-1 hover:bg-amber-200" v-for="(link,i) in navLinks" :key="i">
+                    <router-link :to="link.path">{{ link.text }}</router-link>
+                </li>
+            </ul>
+        </div>
+    </nav>
+  </header>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            navLinks: [
+                {
+                    text: 'home',
+                    path: '/'
+                },
+                {
+                    text: 'Chi siamo',
+                    path: '/chi-siamo'
+                },
+                {
+                    text: 'Blog',
+                    path: '/blog'
+                },
+                {
+                    text: 'Contatti',
+                    path: '/contatti'
+                },
+            ]
+        }
+    }
+
+}
+</script>
+
+<style lang="scss" scoped> 
+
+</style>
